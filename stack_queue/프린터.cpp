b5c686c00f4f537bb39682bc94974de0dd0df3be
2016024820 <honeda121@hanyang.ac.kr>
2021-07-01 13:42:36 +0900

    algorithm 헤더를 한 번 공부하자.

--- stack_queue/프린터.cpp
+++ stack_queue/프린터.cpp
@@ -6,41 +6,49 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    int temporary = 0;
+    int firstElement = 0;
     
     int i = 0;
+    //int numberOfPop = 0;
     bool isMax = true;
     while(priorities.size() > 0){
-        temporary = priorities.front();
+        firstElement = priorities.front();
         isMax = true;
         
         for(int j = 1; j<priorities.size(); j++){
             priorities[j-1] = priorities[j];
-            if(priorities[j]>temporary){
+            if(priorities[j]>firstElement){
                 isMax = false;
             }
         }
-        priorities.back() = temporary;
+        priorities.back() = firstElement;
         
-        if(isMax){
-            cout<<temporary<<" ";
-            priorities.pop_back();
+        if(i == location && !isMax){
+            location += priorities.size();
+        }else if(i == location && isMax){
             answer++;
-        }
-        //판별
-        /*
-        if(i == location){
             break;
         }
+        
+
+        if(isMax){
+            //cout<<firstElement<<" ";
+            priorities.pop_back();
+            answer++;
+            //numberOfPop++;
+        }
+        //판별
+
         i++;
-        */
     }
+    cout << endl;
     return answer;
 }
 
 int main (){
-    vector<int> v = {2,1,3,2};
+    vector<int> v1 = {2,1,3,2};
+    vector<int> v2 = {1,1,9,1,1,1};
 
-    cout<<"answer: "<<solution(v,2)<<endl;
+    cout<<"answer: "<<solution(v2,5)<<endl;
     return 0;
 }
